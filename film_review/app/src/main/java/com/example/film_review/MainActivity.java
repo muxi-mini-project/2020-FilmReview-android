ee58b40480cb8acb9cb1a43373a68ed206517988
wenxin <193367189@qq.com>
2020-01-17 15:56:00 +0800

    温鑫first

--- film_review/app/src/main/java/com/example/film_review/MainActivity.java
+++ film_review/app/src/main/java/com/example/film_review/MainActivity.java
@@ -3,16 +3,25 @@ package com.example.film_review;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView btn_personal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Fresco.initialize(this);
         FragmentManager fragmentManager=getSupportFragmentManager();
         Fragment fragment= fragmentManager.findFragmentById(R.id.fragment_container);
 
@@ -20,5 +29,15 @@ public class MainActivity extends AppCompatActivity {
             fragment=new fragment_firstpage();
             fragmentManager.beginTransaction().add(R.id.fragment_container,fragment).commit();
         }
+        btn_personal=(ImageView)findViewById(R.id.personal);
+        btn_personal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                Fragment fragment= fragmentManager.findFragmentById(R.id.fragment_container);
+                    fragment = new fragment_personal();
+                    fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            }
+        });
     }
 }
---
+++ film_review/app/src/main/java/com/example/film_review/fragment_personal.java
@@ -0,0 +1,29 @@
+package com.example.film_review;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+
+public class fragment_personal extends Fragment {
+    private SimpleDraweeView msimpledraweeView;
+    @Override
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_personal, container,false);
+    }
+    public void initView()
+    {
+        msimpledraweeView= msimpledraweeView.findViewById(R.id.avatar);
+        //   mTabs=findViewById(R.id.tabs);
+        //   mMyadapter=new Myadapter(getSupportFragmentManager());
+        Uri imageUri= Uri.parse("http://placekitten.com/300/200");
+        msimpledraweeView.setImageURI(imageUri);
+    }
+}
